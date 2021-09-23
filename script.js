a66bfb7323898262af90a90ce451abd1fb9b39bf
Edrei <edreimateus@gmail.com>
2021-09-23 15:04:54 -0300

    feat: some css changes

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const gallery = document.querySelector('.row');
 
-
 function transformDate (date) {
   const arr = [...date];
   const transformDate = `${[arr[8]]}${[arr[9]]}/${[arr[5]]}${[arr[6]]}/${[arr[0]]}${[arr[1]]}${[arr[2]]}${[arr[3]]}`;
@@ -27,9 +26,15 @@ async function getImages () {
   loading()
   const fetchImages = await fetch('https://us-central1-squid-apis.cloudfunctions.net/test-front-basic');
   const result =  await fetchImages.json();
-  console.log(result)
+
   result.forEach((item) =>{
-    const { comentarios, criadoEm, link, metadados: { engagement }, imagens: { thumbnail: { url } }, usuario: { username } } = item;
+
+    const { 
+      comentarios, criadoEm, link, 
+      metadados: { engagement }, 
+      imagens: { thumbnail: { url } }, 
+      usuario: { username } } = item;
+
     const createDiv = document.createElement('div');
     createDiv.className = "box";
     createDiv.innerHTML = `<a href=${link} target="_blank">
@@ -40,8 +45,10 @@ async function getImages () {
     <p><i class="fas fa-heart">  ${engagement}</i></p>
     <p><i class="fas fa-calendar-alt">  ${transformDate(criadoEm)}</i></p>
     </div></a>`;
+    
     gallery.appendChild(createDiv);
-  })
+  });
+
   removeLoading();
 }
 
